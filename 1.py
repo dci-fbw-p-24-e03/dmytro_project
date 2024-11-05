d263074ee9548bcaf9631d5ae515cd78950c22f2
hahwyd <hahwyd@gmail.com>
2024-11-06 00:37:41 +0100

    123

---
+++ 1.py
@@ -0,0 +1,9 @@
+from currency_converter import CurrencyConverter
+from tkinter import *
+
+root = Tk()
+root.title("Currency converter")
+root.geometry("300x250+300+300")
+root.resizable(width=False, height=False)
+root["bg"] = "black"
+c = 
